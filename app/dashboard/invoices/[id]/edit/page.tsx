d6c5fc09fe7ce79cb149e0244579b1a3f178128b
william-lim-wen-bin <132965638+william-lim-wen-bin@users.noreply.github.com>
2025-02-26 15:18:36 +0800

    [chap 12] Read the invoice id from page params

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,9 @@
+import Form from "@/app/ui/invoices/edit-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers } from "@/app/lib/data";
+
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const id = params.id;
+  // ...
+}
